aae672eeebbd168f43f0e00db956193bacfa2e14
js899 <49071268+js899@users.noreply.github.com>
2019-06-22 12:42:09 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,17 +1,18 @@
+#webscraping+python+selenium+webdriver
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait 
-#from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait #makes the driver wait until the whole page is loaded
+#from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys         #used to send keyboard keys
+from selenium.webdriver.common.by import By             #to select elements from webpage source code(inspect)
 #import time
 driver = webdriver.Chrome("C:\\Users\\hp\\mydriver\\chromedriver")
 driver.get("https://login.gndec.ac.in/nebtree/login.php ")
-username = driver.find_element_by_id("------")
-username.clear()
-username.send_keys("*****")
+username = driver.find_element_by_id("------")          #username input box
+username.clear()                                          
+username.send_keys("*****")                             #username keys sent
 password = driver.find_element_by_id("-----")
 password.clear()
 password.send_keys("*****") 
-driver.find_element_by_name("submit").click()
-driver.close()
+driver.find_element_by_name("submit").click()           #submit button clicked
+driver.close()                                          #browser closed
 #send_keys(Keys.ENTER)
